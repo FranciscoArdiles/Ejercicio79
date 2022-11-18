ff16a31c5ecaf874c47d47553456b3d11c87ed44
Francisco <jfranciscoardiles@gmail.com>
2022-11-18 01:01:29 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio79/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+
+            SortedSet<string> datos = new SortedSet<string>();
+
+            Console.WriteLine("Ingrese una frase o ingrese ¨salir¨ para salir");
+            cadena = Console.ReadLine();
+
+            while (cadena != "salir")
+            {
+                if (datos.Contains(cadena))
+                {
+                    Console.WriteLine("Esa frase ya existe!");
+                }
+                datos.Add(cadena);
+                Console.WriteLine("Ingrese la siguente frase o ingrese ¨salir¨ para salir");
+                cadena = Console.ReadLine();
+            }
+        }
+    }
+}
